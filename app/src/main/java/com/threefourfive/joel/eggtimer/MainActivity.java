a44c2d3de3376228990283f0de417cd4239ae7b4
joelbandi <joelvivekbandi@gmail.com>
2016-01-03 03:45:00 -0800

    "intial commit"

--- app/src/main/java/com/threefourfive/joel/eggtimer/MainActivity.java
+++ app/src/main/java/com/threefourfive/joel/eggtimer/MainActivity.java
@@ -1,16 +1,24 @@
 package com.threefourfive.joel.eggtimer;
 
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
     TextView textview;
     SeekBar seekbar;
+    CountDownTimer count;
+    MediaPlayer mplayer = MediaPlayer.create(getApplicationContext(),R.raw.ping);
+    boolean active = false;
 
 
     public void updatetimer(int progress){
@@ -26,21 +34,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void resettimer(){
+        button.setText("Go!");
+        seekbar.setProgress(30);
+        seekbar.setVisibility(VISIBLE);
+        updatetimer(seekbar.getProgress());
+    }
 
-    public void go(){
-        
 
-        new CountDownTimer(seekbar.getProgress()*1000 + 100,1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                updatetimer((int)millisUntilFinished/1000);
-            }
+    public void go(View view){
 
-            @Override
-            public void onFinish() {
+        if (!active) {
+            seekbar.setVisibility(INVISIBLE);
+            active = true;
+            button.setText("Stop!");
+            count = new CountDownTimer(seekbar.getProgress() * 1000 + 100, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    updatetimer((int) millisUntilFinished / 1000);
+                }
 
-            }
-        };
+                @Override
+                public void onFinish() {
+                    mplayer.start();
+                    resettimer();
+                }
+            }.start();
+        }
+        else{
+            resettimer();
+        }
     }
 
     @Override
